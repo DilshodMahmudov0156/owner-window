005b3d7bc9f8a0b07608a2222b92d6bc4fd246f7
Dilshod Mahmudov <dilshodgithub0156@gmail.com>
2024-07-29 22:59:02 +0500

    I changed settings part styles

--- src/parts/settings-page.js
+++ src/parts/settings-page.js
@@ -7,9 +7,9 @@ function SettingsPage() {
             <div className="d-flex justify-content-end">
                 <button className="add-btn">+ Yangi qo'shish</button>
             </div>
-            <div className="row justify-content-center add-window mt-4">
-                <div className="col-lg-4">
-                    <div className="card">
+            <div className="row justify-content-evenly add-window mt-4">
+                <div className="col-xl-4 col-sm-6">
+                    <div className="">
                         <form action="" className="card-body">
                             <label htmlFor="image" className="my-image-label">
                                 <i className="bi bi-image"></i>
@@ -22,18 +22,27 @@ function SettingsPage() {
                             <input type="text" className="my-input" placeholder="Mahsulot haqida" id='commit'/>
                             <label htmlFor="price" className="my-label">Narxi:</label>
                             <input type="number" className="my-input" placeholder="Mahsulot narxi" id="price"/>
-                            <div className="text-end">
-                                <button className="submit-btn">Mahsulotni Joylash</button>
+                            <div className="text-center">
+                                <button className="submit-btn">
+                                    Mahsulotni Joylash
+                                </button>
                             </div>
                         </form>
                     </div>
                 </div>
 
-                <div className="col-lg-4">
-                    <div className="card">
-                        <div className="my-card shadow">
-                            <img src={pitsa} alt="" className="card-image"/>
+                <div className="col-xl-4 col-sm-6">
+                    <div className="my-card shadow-lg">
+                        <img src={pitsa} alt="mahsulot-rasmi" className="card-image"/>
+                        <div className="d-flex justify-content-between mt-2">
+                            <h4>Title</h4>
+                            <h5>
+                                <span className="price">25,000</span> sum
+                            </h5>
                         </div>
+                        <p className="info">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, eligendi!
+                        </p>
                     </div>
                 </div>
             </div>
